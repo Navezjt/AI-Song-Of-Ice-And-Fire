67e420e3d154635fe1ef8b16fffb0225596d3154
Liam Swayne <108629034+LiamSwayne@users.noreply.github.com>
2023-07-09 19:09:10 -0400

    Add files via upload

---
+++ chatToBook.py
@@ -0,0 +1,43 @@
+# re-format markdown chat to book
+
+def flatten(lst):
+    flattened_list = []
+    for item in lst:
+        if isinstance(item, list):
+            flattened_list.extend(flatten(item))
+        else:
+            flattened_list.append(item)
+    return flattened_list
+
+def formatChat(chat):
+    chunks = chat.split("ChatGPT: \n- ")
+    for i in range(len(chunks)):
+        while chunks[i][0] == "\n":
+            chunks[i] = chunks[i][1:]
+        chunks[i] = chunks[i].split("\n")
+
+    chunks = flatten(chunks)
+    newChunks = []
+    for i in range(len(chunks)):
+        if len(chunks[i]) > 0:
+            while chunks[i][0] == "\n":
+                chunks[i] = chunks[i][1:]
+            
+            if chunks[i][0:7] != "Person:" and chunks[i][0:16] != "- Your responses":
+                newChunks.append(chunks[i])
+
+    for i in range(len(newChunks)):
+        if newChunks[i][:2] == "- ":
+            newChunks[i] = newChunks[i][2:]
+    
+    return newChunks
+
+chat = '''
+'''
+
+chunks = formatChat(chat)
+
+
+for text in chunks:
+    print()
+    print(text)
