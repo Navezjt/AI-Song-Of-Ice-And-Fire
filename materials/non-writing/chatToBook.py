e772318d69022d5299d649a6e9c72937b7066c9d
Liam Swayne <108629034+LiamSwayne@users.noreply.github.com>
2023-07-09 20:03:23 -0400

    Add files via upload

--- materials/non-writing/chatToBook.py
+++ materials/non-writing/chatToBook.py
@@ -38,6 +38,8 @@ chat = '''
 chunks = formatChat(chat)
 
 for i in range(len(chunks)):
-    print(chunks[i])
+    if i % 4 == 0:
+        print('I will provide you with a passage from a book. Rewrite the passage to remove all references that break the fourth wall, including references to "the reader" or "the chapter" or "the story". Make the passage sound more like it was written by George R R Martin. Create paragraph breaks in the passage. Respond with nothing but your modified version of the passage. Here is the passage: '+chunks[i]+' '+chunks[i+1]+' '+chunks[i+2]+' '+chunks[i+3])
+
     if i % 4 == 3:
         print()
